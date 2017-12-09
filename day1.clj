a167b508cdf61003eaa91bab5c9c2dddea1deab1
Alexander C Rees <me@acrees.co.uk>
2017-12-09 12:39:34 +0000

    1-2

--- day1.clj
+++ day1.clj
@@ -1,4 +1,14 @@
-(defn sumx [x]
+(defn split-numbers [x]
+  (map read-string
+       (clojure.string/split x #"")))
+
+(defn next-index [length current]
+  (let [i (+ (/ length 2) current)]
+    (if (< i length) i (- i length))))
+
+(defn sum [x] (reduce + x))
+
+(defn sum1 [x]
   (last
     (reduce
       (fn [[last total] n]
@@ -6,7 +16,18 @@
               [n (+ total n)]
               [n total]))
       [0 0]
-      (let [xs (clojure.string/split x #"")]
-           (map read-string (conj xs (first xs)))))))
+      (let [xs (split-numbers x)]
+           (concat xs (take 1 xs))))))
 
-(sumx "1212")
+(defn sum2 [x]
+  (let [xs (vec (split-numbers x))
+        len (count xs)]
+        (sum
+            (map-indexed
+                (fn [i item]
+                  (let [j (next-index len i)
+                        next-item (nth xs j)]
+                    (if (= item next-item) item 0)))
+              xs))))
+
+(sum2 "1212")
